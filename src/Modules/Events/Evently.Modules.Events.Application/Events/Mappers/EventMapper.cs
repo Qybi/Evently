fb4a33da5041c3820b9517c5521048c40d4a3503
Qybi <raffaele.trivillin@gmail.com>
2026-07-24 21:01:13 +0200

    feat: add EventMapper

---
+++ src/Modules/Events/Evently.Modules.Events.Application/Events/Mappers/EventMapper.cs
@@ -0,0 +1,16 @@
+﻿using Evently.Modules.Events.Application.Events.DTO;
+using Evently.Modules.Events.Domain.Events;
+using Riok.Mapperly.Abstractions;
+
+namespace Evently.Modules.Events.Application.Events.Mappers;
+
+[Mapper]
+public static partial class EventMapper
+{
+    // The decorator lets Mapperly map properties ignoring non existent properties on target that do exist on source
+    //[MapperRequiredMapping(RequiredMappingStrategy.Target)]
+
+    [MapperIgnoreSource(nameof(Event.Status))]
+    public static partial EventResponseDto ToResponseDto(this Event @event);
+    public static partial IQueryable<EventResponseDto> ProjectToDto(this IQueryable<Event> events);
+}
