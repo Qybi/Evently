d3131d2b09c27763946c1f60b68f6a4be04b2052
Qybi <raffaele.trivillin@gmail.com>
2026-07-24 16:46:42 +0200

    feat: add EventRepository

---
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/Events/EventRepository.cs
@@ -0,0 +1,12 @@
+﻿using Evently.Modules.Events.Api.Database;
+using Evently.Modules.Events.Domain.Events;
+
+namespace Evently.Modules.Events.Infrastructure.Events;
+
+internal sealed class EventRepository(EventsDbContext context) : IEventRepository
+{
+    public void Insert(Event @event)
+    {
+        context.Events.Add(@event);
+    }
+}
