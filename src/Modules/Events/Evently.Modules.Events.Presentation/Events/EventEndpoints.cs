8309272875474194c1a43944d48b52419e117eda
Qybi <raffaele.trivillin@gmail.com>
2026-07-24 16:41:48 +0200

    feat: add events endpoints to presentation layer

---
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Events/EventEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Evently.Modules.Events.Api.Events;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Presentation.Events;
+
+public static class EventEndpoints
+{
+    public static void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        CreateEvent.MapEndpoint(app);
+        GetEvent.MapEndpoint(app);
+    }
+}
---
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Events/GetEvent.cs
@@ -0,0 +1,21 @@
+﻿using Evently.Modules.Events.Application.Events;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Api.Events;
+
+internal static class GetEvent
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("events/{id}", async (Guid id, ISender sender) =>
+        {
+            var query = new GetEventQuery(id);
+            EventResponse? @event = await sender.Send(query);
+
+            return @event is null ? Results.NotFound() : Results.Ok(@event);
+        }).WithTags(Tags.Events);
+    }
+}
