eff8b23b962e1c7b9b56bc6c4a206466071349ca
Qybi <raffaele.trivillin@gmail.com>
2026-07-24 20:57:38 +0200

    feat: add EventResponseDto

---
+++ src/Modules/Events/Evently.Modules.Events.Application/Events/DTO/EventResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Evently.Modules.Events.Application.Events.DTO;
+
+public sealed record EventResponseDto(
+    Guid Id,
+    string Title,
+    string Description,
+    string Location,
+    DateTime StartsAtUtc,
+    DateTime? EndsAtUtc
+);
