2def27c454ccde759617b35a0cd62a1d76561fdf
Qybi <raffaele.trivillin@gmail.com>
2026-07-24 16:40:26 +0200

    feat: add events module to infrastructure project

---
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Evently.Modules.Events.Api.Database;
+using Evently.Modules.Events.Api.Events;
+using Evently.Modules.Events.Application.Events;
+using Evently.Modules.Events.Presentation.Events;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Evently.Modules.Events.Api;
+
+public static class EventsModule
+{
+    public static void MapEndpoints(IEndpointRouteBuilder app)
+    {
+        EventEndpoints.MapEndpoints(app);
+    }
+
+    public static IServiceCollection AddEventsModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        string databaseConnectionString = configuration.GetConnectionString("Database")!;
+
+        services.AddDbContext<EventsDbContext>(
+            options => options.UseNpgsql(
+                databaseConnectionString, 
+                npgsqlOptions => npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Events)
+            )
+            .UseSnakeCaseNamingConvention()
+        );
+
+        return services;
+    }
+}
