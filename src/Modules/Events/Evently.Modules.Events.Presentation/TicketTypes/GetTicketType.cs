2452b784b15cd6762ce409c28b3fe0393e54f6f9
Qybi <raffaele.trivillin@gmail.com>
2026-07-31 22:46:32 +0200

    feat: add category and ticket type endpoints

---
+++ src/Modules/Events/Evently.Modules.Events.Presentation/TicketTypes/GetTicketType.cs
@@ -0,0 +1,24 @@
+﻿using Evently.Modules.Events.Application.TicketTypes.Queries.GetTicketType;
+using Evently.Modules.Events.Application.TicketTypes.Queries.ViewModels;
+using Evently.Modules.Events.Domain.Abstractions;
+using Evently.Modules.Events.Presentation.ApiResults;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Evently.Modules.Events.Presentation.TicketTypes;
+
+internal static class GetTicketType
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("ticket-types/{id}", async (Guid id, ISender sender) =>
+        {
+            Result<TicketTypeViewModel> result = await sender.Send(new GetTicketTypeQuery(id));
+
+            return result.Match(Results.Ok, ApiResults.ApiResults.Problem);
+        })
+        .WithTags(Tags.TicketTypes);
+    }
+}
