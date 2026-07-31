d6915e437b96cfa0573317047597784140944c95
Qybi <raffaele.trivillin@gmail.com>
2026-07-31 22:46:35 +0200

    di: register repositories, queries and module endpoints

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
@@ -1,14 +1,23 @@
 ﻿using Evently.Modules.Events.Api.Database;
+using Evently.Modules.Events.Application.Abstractions.Clock;
 using Evently.Modules.Events.Application.Abstractions.Data;
+using Evently.Modules.Events.Application.Categories;
 using Evently.Modules.Events.Application.Events;
-using Evently.Modules.Events.Infrastructure.Events;
+using Evently.Modules.Events.Application.TicketTypes;
+using Evently.Modules.Events.Infrastructure.Clock;
+using Evently.Modules.Events.Infrastructure.Database;
+using Evently.Modules.Events.Infrastructure.Queries;
+using Evently.Modules.Events.Infrastructure.Repositories;
+using Evently.Modules.Events.Presentation.Categories;
 using Evently.Modules.Events.Presentation.Events;
+using Evently.Modules.Events.Presentation.TicketTypes;
 using FluentValidation;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Evently.Modules.Events.Infrastructure;
 
@@ -17,6 +26,8 @@ public static class EventsModule
     public static void MapEndpoints(IEndpointRouteBuilder app)
     {
         EventEndpoints.MapEndpoints(app);
+        CategoryEndpoints.MapEndpoints(app);
+        TicketTypeEndpoints.MapEndpoints(app);
     }
 
     public static IServiceCollection AddEventsModule(this IServiceCollection services, IConfiguration configuration)
@@ -45,11 +56,17 @@ public static class EventsModule
                 npgsqlOptions => npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Events)
             )
             .UseSnakeCaseNamingConvention()
+            .AddInterceptors()
         );
 
-        services.AddScoped<IEventRepository, EventRepository>();
-        services.AddSingleton<IEventQueries, EventQueries>();
-
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<EventsDbContext>());
+
+        services.AddScoped<IEventRepository, EventRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<ITicketTypeRepository, TicketTypeRepository>();
+
+        services.AddScoped<IEventQueries, EventQueries>();
+        services.AddScoped<ICategoryQueries, CategoryQueries>();
+        services.AddScoped<ITicketTypeQueries, TicketTypeQueries>();
     }
 }
