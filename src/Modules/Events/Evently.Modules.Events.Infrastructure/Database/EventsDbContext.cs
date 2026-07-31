a5d0072f1bca95a003a21483cfdb95b73e4b42b4
Qybi <raffaele.trivillin@gmail.com>
2026-07-31 22:46:24 +0200

    feat: add entity configurations for events and ticket types

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventsDbContext.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventsDbContext.cs
@@ -1,5 +1,9 @@
 ﻿using Evently.Modules.Events.Application.Abstractions.Data;
+using Evently.Modules.Events.Domain.Categories;
 using Evently.Modules.Events.Domain.Events;
+using Evently.Modules.Events.Domain.TicketTypes;
+using Evently.Modules.Events.Infrastructure.Database;
+using Evently.Modules.Events.Infrastructure.Database.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace Evently.Modules.Events.Api.Database;
@@ -7,8 +11,14 @@ namespace Evently.Modules.Events.Api.Database;
 public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options) : DbContext(options), IUnitOfWork
 {
     internal DbSet<Event> Events { get; set; }
+    internal DbSet<Category> Categories { get; set; }
+    internal DbSet<TicketType> TicketTypes { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Events);
+
+        modelBuilder.ApplyConfiguration(new EventConfiguration());
+        modelBuilder.ApplyConfiguration(new TicketTypeConfiguration());
     }
 }
