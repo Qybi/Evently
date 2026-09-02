024dd07bbde6ac63b07af3f65fe63b11b6e807e5
Qybi <raffaele.trivillin@gmail.com>
2026-09-02 15:52:33 +0200

    feat: add cache check on GetCategories endpoint

--- src/Modules/Events/Evently.Modules.Events.Presentation/Categories/GetCategories.cs
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Categories/GetCategories.cs
@@ -1,6 +1,7 @@
 ﻿using Evently.Modules.Events.Application.Categories.Queries.GetCategories;
 using Evently.Modules.Events.Application.Categories.Queries.ViewModels;
 using Evently.Modules.Events.Presentation.ApiResults;
+using Evently.Shared.Application.Caching;
 using Evently.Shared.Domain;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
@@ -13,9 +14,21 @@ internal static class GetCategories
 {
     public static void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("categories", async (ISender sender) =>
+        app.MapGet("categories", async (ISender sender, ICacheService cacheService) =>
         {
+            IReadOnlyCollection<CategoryViewModel> cachedCategories = await cacheService.GetAsync<IReadOnlyCollection<CategoryViewModel>>("categories");
+
+            if (cachedCategories is not null)
+            {
+                return Results.Ok(cachedCategories);
+            }
+
             Result<IReadOnlyCollection<CategoryViewModel>> result = await sender.Send(new GetCategoriesQuery());
+
+            if (result.IsSuccess)
+            {
+                await cacheService.SetAsync("categories", result.Value);
+            }
 
             return result.Match(Results.Ok, ApiResults.ApiResults.Problem);
         })
