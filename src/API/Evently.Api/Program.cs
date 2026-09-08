61d658eff8295fd22650a44cf64dc17dfdc7b1d6
Qybi <raffaele.trivillin@gmail.com>
2026-09-08 09:52:47 +0200

    feat: scaffold ticketing module

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -1,6 +1,7 @@
 using Evently.Api.Extensions;
 using Evently.Api.Middleware;
 using Evently.Modules.Events.Infrastructure;
+using Evently.Modules.Ticketing.Infrastructure;
 using Evently.Modules.Users.Infrastructure;
 using Evently.Shared.Application;
 using Evently.Shared.Infrastructure;
@@ -34,7 +35,8 @@ builder.Services.AddOpenApi(options =>
 
 builder.Services.AddApplication([
     Evently.Modules.Events.Application.AssemblyReference.Assembly,
-    Evently.Modules.Users.Application.AssemblyReference.Assembly
+    Evently.Modules.Users.Application.AssemblyReference.Assembly,
+    Evently.Modules.Ticketing.Application.AssemblyReference.Assembly
 ]);
 
 string databaseConnectionString = builder.Configuration.GetConnectionString("Database")!;
@@ -42,7 +44,7 @@ string cacheConnectionString = builder.Configuration.GetConnectionString("Cache"
 
 builder.Services.AddInfrastructure(cacheConnectionString);
 
-builder.Configuration.AddModuleConfiguration(["events", "users"]);
+builder.Configuration.AddModuleConfiguration(["events", "users", "ticketing"]);
 
 builder.Services.AddHealthChecks()
     .AddNpgSql(databaseConnectionString)
@@ -50,6 +52,7 @@ builder.Services.AddHealthChecks()
 
 builder.Services.AddEventsModule(builder.Configuration);
 builder.Services.AddUsersModule(builder.Configuration);
+builder.Services.AddTicketingModule(builder.Configuration);
 
 WebApplication app = builder.Build();
 
---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Evently.Modules.Ticketing.Infrastructure;
+
+public static class TicketingModule
+{
+    public static IServiceCollection AddTicketingModule(
+    this IServiceCollection services,
+    IConfiguration configuration)
+    {
+        services.AddInfrastructure(configuration);
+        services.AddEndpoints(Presentation.AssemblyReference.Assembly);
+
+        return services;
+    }
+
+    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<UsersDbContext>(
+            (sp, options) => options
+                .UseNpgsql(
+                    configuration.GetConnectionString("Database"),
+                    npgsqlOptions => npgsqlOptions
+                        .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Users))
+                .UseSnakeCaseNamingConvention()
+                .AddInterceptors(sp.GetRequiredService<PublishDomainEventsInterceptor>()));
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<UsersDbContext>());
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddScoped<IUserQueries, UserQueries>();
+    }
+}
