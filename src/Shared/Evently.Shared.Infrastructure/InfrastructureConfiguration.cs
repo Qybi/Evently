3e578fb63b23ff4d5add36dc8b16efbab194e78d
Qybi <raffaele.trivillin@gmail.com>
2026-09-18 11:10:54 +0200

    feat: add authentication and authorization configuration and endpoints guard

--- src/Shared/Evently.Shared.Infrastructure/InfrastructureConfiguration.cs
+++ src/Shared/Evently.Shared.Infrastructure/InfrastructureConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Evently.Shared.Application.Caching;
 using Evently.Shared.Application.Clock;
 using Evently.Shared.Application.EventBus;
+using Evently.Shared.Infrastructure.Authentication;
 using Evently.Shared.Infrastructure.Caching;
 using Evently.Shared.Infrastructure.Clock;
 using Evently.Shared.Infrastructure.Interceptors;
@@ -15,6 +16,8 @@ public static class InfrastructureConfiguration
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, Action<IRegistrationConfigurator>[] moduleConfigureConsumers, string redisConnectionString)
     {
+        services.AddAuthenticationInternal();
+
         services.TryAddSingleton<PublishDomainEventsInterceptor>();
 
         services.TryAddSingleton<IDateTimeProvider, DateTimeProvider>();
