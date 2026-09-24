98bf3dbdbd5afb536902fe29abcaa7df1ac0f368
Qybi <raffaele.trivillin@gmail.com>
2026-09-24 14:44:48 +0200

    test: add events module architecture tests

---
+++ test/Modules/Events/Evently.Tests.Modules.Events.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Evently.Modules.Events.Domain.Categories;
+using Evently.Modules.Events.Infrastructure;
+
+namespace Evently.Tests.Modules.Events.ArchitectureTests.Abstractions;
+
+#pragma warning disable CA1515 // Consider making public types internal
+public abstract class BaseTest
+#pragma warning restore CA1515 // Consider making public types internal
+{
+    protected static readonly Assembly ApplicationAssembly = typeof(Evently.Modules.Events.Application.AssemblyReference).Assembly;
+
+    protected static readonly Assembly DomainAssembly = typeof(Category).Assembly;
+
+    protected static readonly Assembly InfrastructureAssembly = typeof(EventsModule).Assembly;
+
+    protected static readonly Assembly PresentationAssembly = typeof(Evently.Modules.Events.Presentation.AssemblyReference).Assembly;
+}
