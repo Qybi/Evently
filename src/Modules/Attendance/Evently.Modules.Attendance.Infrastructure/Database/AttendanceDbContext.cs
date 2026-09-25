1094457b9f4fd568cef9506b0efd88c0d62a7c23
Qybi <raffaele.trivillin@gmail.com>
2026-09-25 11:29:28 +0200

    feat: add outbox messages table to attendance schema

--- src/Modules/Attendance/Evently.Modules.Attendance.Infrastructure/Database/AttendanceDbContext.cs
+++ src/Modules/Attendance/Evently.Modules.Attendance.Infrastructure/Database/AttendanceDbContext.cs
@@ -3,6 +3,7 @@ using Evently.Modules.Attendance.Domain.Attendees;
 using Evently.Modules.Attendance.Domain.Events;
 using Evently.Modules.Attendance.Domain.Tickets;
 using Evently.Modules.Attendance.Infrastructure.Database.Configurations;
+using Evently.Shared.Infrastructure.Outbox;
 using Microsoft.EntityFrameworkCore;
 
 namespace Evently.Modules.Attendance.Infrastructure.Database;
@@ -17,6 +18,8 @@ public sealed class AttendanceDbContext(DbContextOptions<AttendanceDbContext> op
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Attendance);
+
+        modelBuilder.ApplyConfiguration(new OutboxMessageConfiguration());
 
         modelBuilder.ApplyConfiguration(new AttendeeConfiguration());
         modelBuilder.ApplyConfiguration(new EventConfiguration());
