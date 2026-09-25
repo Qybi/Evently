630a8776a95736269306fa2ce4dce6ccf508f39e
Qybi <raffaele.trivillin@gmail.com>
2026-09-25 16:28:44 +0200

    feat: publish event published integration event

---
+++ src/Modules/Events/Evently.Modules.Events.Application/Events/DomainEventHandlers/EventPublishedDomainEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using Evently.Modules.Events.Application.Events.Queries.GetEvent;
+using Evently.Modules.Events.Application.Events.Queries.ViewModels;
+using Evently.Modules.Events.Domain.Events.DomainEvents;
+using Evently.Modules.Events.IntegrationEvents;
+using Evently.Shared.Application.EventBus;
+using Evently.Shared.Application.Exceptions;
+using Evently.Shared.Application.Messaging;
+using Evently.Shared.Domain;
+using MediatR;
+
+namespace Evently.Modules.Events.Application.Events.DomainEventHandlers;
+
+internal sealed class EventPublishedDomainEventHandler(ISender sender, IEventBus eventBus)
+    : DomainEventHandler<EventPublishedDomainEvent>
+{
+    public override async Task Handle(
+        EventPublishedDomainEvent domainEvent,
+        CancellationToken cancellationToken = default)
+    {
+        Result<EventViewModel> result = await sender.Send(new GetEventQuery(domainEvent.EventId), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            throw new EventlyException(nameof(GetEventQuery), result.Error);
+        }
+
+        await eventBus.PublishAsync(
+            new EventPublishedIntegrationEvent(
+                domainEvent.Id,
+                domainEvent.OccurredOnUtc,
+                result.Value.Id,
+                result.Value.Title,
+                result.Value.Description,
+                result.Value.Location,
+                result.Value.StartsAtUtc,
+                result.Value.EndsAtUtc,
+                result.Value.TicketTypes.Select(t => new TicketTypeModel
+                {
+                    Id = t.TicketTypeId,
+                    EventId = result.Value.Id,
+                    Name = t.Name,
+                    Price = t.Price,
+                    Currency = t.Currency,
+                    Quantity = t.Quantity
+                }).ToList()),
+            cancellationToken);
+    }
+}
