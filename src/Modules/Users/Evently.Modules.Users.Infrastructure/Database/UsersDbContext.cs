1ecac47e466074c4b79f0b9e5ab3326aea5b26f8
Qybi <raffaele.trivillin@gmail.com>
2026-09-25 11:04:42 +0200

    feat: add outbox messages table to users schema

--- src/Modules/Users/Evently.Modules.Users.Infrastructure/Database/UsersDbContext.cs
+++ src/Modules/Users/Evently.Modules.Users.Infrastructure/Database/UsersDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Evently.Modules.Users.Application.Abstractions.Data;
 using Evently.Modules.Users.Domain.Users;
 using Evently.Modules.Users.Infrastructure.Database.Configurations;
+using Evently.Shared.Infrastructure.Outbox;
 using Microsoft.EntityFrameworkCore;
 
 namespace Evently.Modules.Users.Infrastructure.Database;
@@ -12,6 +13,8 @@ public sealed class UsersDbContext(DbContextOptions<UsersDbContext> options) : D
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Users);
+
+        modelBuilder.ApplyConfiguration(new OutboxMessageConfiguration());
 
         modelBuilder.ApplyConfiguration(new UserConfiguration());
         modelBuilder.ApplyConfiguration(new RoleConfiguration());
