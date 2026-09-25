bf74d324b2c2180d131823eae9a5d027dd92bfa4
Qybi <raffaele.trivillin@gmail.com>
2026-09-25 16:29:00 +0200

    feat: add event canceled domain event handlers

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Application/Events/DomainEventHandlers/RefundPaymentsEventCanceledDomainEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using Evently.Modules.Ticketing.Application.Payments.Commands.RefundPaymentsForEvent;
+using Evently.Modules.Ticketing.Domain.Events.DomainEvents;
+using Evently.Shared.Application.Exceptions;
+using Evently.Shared.Application.Messaging;
+using Evently.Shared.Domain;
+using MediatR;
+
+namespace Evently.Modules.Ticketing.Application.Events.DomainEventHandlers;
+
+internal sealed class RefundPaymentsEventCanceledDomainEventHandler(ISender sender)
+    : DomainEventHandler<EventCanceledDomainEvent>
+{
+    public override async Task Handle(
+        EventCanceledDomainEvent domainEvent,
+        CancellationToken cancellationToken = default)
+    {
+        Result result = await sender.Send(new RefundPaymentsForEventCommand(domainEvent.EventId), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            throw new EventlyException(nameof(RefundPaymentsForEventCommand), result.Error);
+        }
+    }
+}
