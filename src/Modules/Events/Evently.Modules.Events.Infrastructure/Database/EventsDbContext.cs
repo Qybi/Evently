0f3cc7fe8531b60be4c98f48c2971f31f829a24b
Qybi <raffaele.trivillin@gmail.com>
2026-09-25 11:29:29 +0200

    feat: add outbox messages table to events schema

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventsDbContext.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventsDbContext.cs
@@ -4,6 +4,7 @@ using Evently.Modules.Events.Domain.Events;
 using Evently.Modules.Events.Domain.TicketTypes;
 using Evently.Modules.Events.Infrastructure.Database;
 using Evently.Modules.Events.Infrastructure.Database.Configurations;
+using Evently.Shared.Infrastructure.Outbox;
 using Microsoft.EntityFrameworkCore;
 
 namespace Evently.Modules.Events.Api.Database;
@@ -17,6 +18,8 @@ public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options) :
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Events);
+
+        modelBuilder.ApplyConfiguration(new OutboxMessageConfiguration());
 
         modelBuilder.ApplyConfiguration(new EventConfiguration());
         modelBuilder.ApplyConfiguration(new TicketTypeConfiguration());
